2e8103140b58fab4ec06d7bdbb06c7442e54ae2d
Chanyeong <dlacksdud2@snu.ac.kr>
2023-05-13 20:59:27 +0900

    c

--- buddy/usermanage/views.py
+++ buddy/usermanage/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.response import Response
+from .models import usermanage
+def CreatePostView(request):
+    realname = request.data.get('realname')
+    userid = request.data.get('userid')
+    content = request.data.get('content')
+    userobj = usermanage.objects.create(realname=realname, userid=userid, content=content)
+    return Response({"msg":f"'{userobj.realnane}'이 등록되었습니다"})
