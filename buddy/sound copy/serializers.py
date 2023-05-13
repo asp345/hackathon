353d4affc6ae1914e8c1c947e0b52177aa4d8687
Chanyeong <dlacksdud2@snu.ac.kr>
2023-05-14 05:51:24 +0900

    del

--- buddy/sound copy/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import sound
-from account.serializers import UserIdUsernameSerializer
-
-class UserSerializer(ModelSerializer):
-    author = UserIdUsernameSerializer(read_only=False)
-    class Meta:
-        model = sound
-        fields = "__all__"
